877cffd3adf5b9be1509da1268873635f53f255c
Volodymyr  Dzhun <volodymyr.dzun@gmail.com>
2023-06-08 14:44:30 +0300

    add interactive icon in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,22 +7,49 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
+  const objectSocialMedia = {
+    instagram: "https://instagram.com/psycholog.boiko?igshid=MzRlODBiNWFlZA==",
+    telegram: "https://t.me/psycholog_boiko",
+    facebook: "https://www.facebook.com/psycholog.vinn",
+  };
+
+  function goToSocialMedia(event) {
+    for (let key in objectSocialMedia) {
+      if (event.target.dataset.icon === key) {
+        window.location.assign(objectSocialMedia[key]);
+      }
+    }
+  }
   return (
     <>
       <div className={style.footer_wraper}>
-        <h1>footer</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-          obcaecati neque reiciendis deleniti quo ab officiis porro facere
-          provident, numquam, repudiandae aut accusantium dolor? Natus libero ab
-          repudiandae fugiat consequatur!
-        </p>
+        <h2 className={style.social_media_icons}>
+          <FontAwesomeIcon
+            icon={faFacebook}
+            onClick={goToSocialMedia}
+            title="Facebook"
+          />
+        </h2>
+        <h2 className={style.social_media_icons}>
+          <FontAwesomeIcon
+            icon={faInstagram}
+            onClick={goToSocialMedia}
+            title="Instagram"
+          />
+        </h2>
+        <h2 className={style.social_media_icons}>
+          <FontAwesomeIcon
+            icon={faTelegram}
+            onClick={goToSocialMedia}
+            title="Telegram"
+          />
+        </h2>
+        <div className={style.allRightProtect_Wraper}>
+          <p className={style.allRightProtect}>
+            &copy; 2023 всі права захижені
+          </p>
+        </div>
       </div>
-      <h1>
-        <FontAwesomeIcon icon={faFacebook} />
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faTelegram} />
-      </h1>
     </>
   );
 }
