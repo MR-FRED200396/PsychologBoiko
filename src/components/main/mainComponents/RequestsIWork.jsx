075acc554ad5d506f10717421700e456252aa6a7
Volodymyr  Dzhun <volodymyr.dzun@gmail.com>
2023-06-17 17:59:20 +0300

    build component RequestsIWork

--- src/components/main/mainComponents/RequestsIWork.jsx
+++ src/components/main/mainComponents/RequestsIWork.jsx
@@ -1,25 +1,95 @@
-import { Row, Col, Typography, List, Card } from "antd";
+import { Row, Col, Typography, Card } from "antd";
+const { Meta } = Card;
 
 export default function RequestsIWork() {
   const requests = [
-    "стосунки з собою",
-    "стосунки з партнером",
-    "особисті кордони",
-    "стосунки з батьками",
-    "самоцінність",
-    "проявленість",
-    "тривога",
-    "образи",
-    "проживання кризи",
-  ];
-  const backgroundImageUrl = "requests.jpg";
+    {
+      alt: "Стосунки з партнером",
+      src: "requests3.jpg",
+      location: "https://www.instagram.com/p/CMhspd6l7__/",
+      title: "Стосунки з партнером",
+      description: "рівноправність, злиття",
+    },
+    {
+      alt: "Тривога",
+      src: "requests2.jpg",
+      location: "https://www.instagram.com/p/CG-jeFoFSc7/",
+      title: "Тривога",
+      description: "відчуття тривоги",
+    },
+    {
+      alt: "Стосунки з батьками",
+      src: "requests7.jpg",
+      location: "https://www.instagram.com/p/CTfK1jJjIKK/",
+      title: "Стосунки з батьками",
+      description: "напорозуміння, сепарація",
+    },
+    {
+      alt: "Проявленість",
+      src: "requests4.jpg",
+      location: "https://www.instagram.com/p/CSZw6Lgj3U8/",
+      title: "Проявленість",
+      description: "заявити про себе",
+    },
+    {
+      alt: "Стосунки з собою",
+      src: "requests9.jpg",
+      location: "https://www.instagram.com/p/CddVSUSNa-g/",
+      title: "Стосунки з собою",
+      description: "шкідливі звички, стрес, немає сил",
+    },
+    {
+      alt: "Невпевненість",
+      src: "requests11.jpg",
+      location: "https://www.instagram.com/p/CoVKynAtfzw/",
+      title: "Невпевненість",
+      description: "невпевненість в собі",
+    },
+    {
+      alt: "Емоційне вигорання",
+      src: "requests1.jpg",
+      location: "https://www.instagram.com/p/CCyi7HFJGRv/",
+      title: "Емоційне вигорання",
+      description: "що це таке? чим небезпечно?",
+    },
 
-  const cardStyle = {
-    backgroundImage: `url(${backgroundImageUrl})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    minheight: "100vh",
-  };
+    {
+      alt: "Підтримка",
+      src: "requests12.jpg",
+      location: "https://www.instagram.com/p/CsD04GnOmGA/",
+      title: "Підтримка",
+      description: "де отримати підтримку?",
+    },
+    {
+      alt: "Дитячі травми",
+      src: "requests6.jpg",
+      location: "https://www.instagram.com/p/CEmBT5EFEbY/",
+      title: "Дитячі травми",
+      description: "чому важливо позбутися?",
+    },
+    {
+      alt: "Обмежуючі переконання",
+      src: "requests10.jpg",
+      location: "https://www.instagram.com/p/CKec1zOletM/",
+      title: "Обмежуючі переконання",
+      description: "чому це важливо?",
+    },
+    {
+      alt: "Криза у відносинах",
+      src: "requests5.jpg",
+      location: "https://www.instagram.com/p/CMxUgnYFdwM/",
+      title: "Криза у відносинах",
+      description: "коли зявляється? як усвідомити?",
+    },
+    {
+      alt: "Панічні атаки",
+      src: "requests8.jpg",
+      location: "https://www.instagram.com/p/CUKpbmwsydt/",
+      title: "Панічні атаки",
+      description: "як побороти і що робити?",
+    },
+  ];
+
   return (
     <>
       <Row>
@@ -34,25 +104,24 @@ export default function RequestsIWork() {
           <Typography.Title level={2}>Запити з якими працюю </Typography.Title>
         </Col>
       </Row>
-      <Row>
-        <Col span={12} style={{ backgroundColor: "#f0f0f0" }}>
-          <List
-            dataSource={requests}
-            renderItem={(item) => (
-              <List.Item
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <li key={item}>{item}</li>
-              </List.Item>
-            )}
-          />
-        </Col>
-        <Col span={12}>
-          <Card style={cardStyle} />
-        </Col>
+
+      <Row justify="space-around">
+        {requests.map((request, index) => (
+          <Card
+            key={index}
+            hoverable
+            style={{
+              width: 240,
+              margin: "10px",
+            }}
+            cover={<img alt={request.alt} src={request.src} />}
+            onClick={() => {
+              window.location.assign(request.location);
+            }}
+          >
+            <Meta title={request.title} description={request.description} />
+          </Card>
+        ))}
       </Row>
     </>
   );
