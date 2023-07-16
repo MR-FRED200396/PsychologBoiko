a1e7c52f763b4de5fe4b3bf7e3afe904d23fda03
Володимир Джунь <volodymyrdzhun@MacBook-Pro-Volodimir.local>
2023-07-16 16:02:31 +0300

    add style and position to button in component HowToGoConsultation

--- src/components/main/mainComponents/HowToGoConsultacion.jsx
+++ src/components/main/mainComponents/HowToGoConsultacion.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ModalComponent from "./modalComponent/ModalComponent";
 
 import { Button, Divider, Row, Col, Typography } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 export default function HowToGoConsultation() {
   const [open, setOpen] = useState(false);
@@ -17,12 +18,29 @@ export default function HowToGoConsultation() {
   return (
     <>
       <Row>
-        <Col span={24}>
+        <Col
+          span={24}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
           <Divider orientation="center" style={{ backgroundColor: "#999" }}>
-            <Typography.Title level={2}>Запис на консультацію</Typography.Title>
+            <Typography.Title level={2}> Хочу на консультацію</Typography.Title>
           </Divider>
-          <Button type="primary" onClick={handleOpen}>
-            Записатись
+          <Button
+            type="primary"
+            onClick={handleOpen}
+            style={{
+              width: "300px",
+              marginLeft: "auto",
+              marginRight: "auto",
+              backgroundColor: "#ff3399",
+            }}
+            shape="round"
+          >
+            Записатись <ArrowRightOutlined />
           </Button>
           <ModalComponent open={open} handleClose={handleClose} />
         </Col>
